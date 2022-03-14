f572acdc2f727adcb96fcf15f9da000a3902b064
sorasasa <101598732+sorasasa@users.noreply.github.com>
2022-03-14 17:27:16 -0300

    Create config.php

---
+++ public_hmtl/config.php
@@ -0,0 +1,8 @@
+<?php
+    define('DBUSER', 'root');
+    define('DBPASSWORD', '');
+    define('DBHOST', 'localhost');
+    define('DBNAME', 'aaaa');
+    define( 'WP_DEBUG', true );
+define( 'WP_DEBUG_LOG', true );
+define( 'WP_DEBUG_DISPLAY', false );
