31a0165e28fa072933f89c3877ecfe2a3884e89c
sorasasa <101598732+sorasasa@users.noreply.github.com>
2022-04-06 15:39:19 -0300

    Update add.js

--- public_hmtl/src/frontend/js/add.js
+++ public_hmtl/src/frontend/js/add.js
@@ -1,63 +1,68 @@
-window.onload = function get_body ()
-Body = document.getElementById('productType')[0];
-elemento_pai.appendChild(body);
-
-el.addEventListener("change", function()
-{var elems = document.querySelectorAll('#noDVD,#noBook,#noFurniture');
-  for (var i = 0; i < elems.length; i++) {elems[i].style.display = 'none'}
-  
-  if (this.selectedIndex === 0) {
-    document.querySelector('#noDVD').style.display = 'block';
-  } else if (this.selectedIndex === 1) {
-    document.querySelector('#noBook').style.display = 'block';
-  }else if (this.selectedIndex === 2) {
-    document.querySelector('#noFurniture').style.display = 'block';
-  }
-}, false);
-
-$(document).ready(() => {
-    $('#type').change(function() {
-        switch (this.value) {
-            case '0':
-                 $('#attributes').html(`
-                    <label for="weight">Weight</label>
-                    <input type="number" step="0.01" class="form-control" id="#weight" required>
-                
-                `);
-                break;
-            case '1':
-                 $('#attributes').html(`
-                    <label for="size">Size</label>
+const getType = productTypeCode => {
+  switch (productTypeCode) {
+    case "DV":
+      return (` <label for="size">Size</label>
                     <input type="number" step="0.01" class="form-control" id="#size" required>
                     
                 `);
-                break;
+    case "BO":
+      return(` <label for="weight">Weight</label>
+                    <input type="number" step="0.01" class="form-control" id="#weight" required>
+                  `);
 
-            case '2':
-                $('#attributes').html(`
-                    <label for="height">Height</label>
+    case "FU":
+      return (` <label for="height">Height</label>
                     <input type="number" step="0.01" class="form-control" id="#height" required>
-                    <label for="width">Width</label>
+                <label for="width">Width</label>
                     <input type="number" step="0.01" class="form-control" id="#width" required>
-                    <label for="length">Length</label>
-                    <input type="number" step="0.01" class="form-control" id="#length" required>
-                    
-                `);
-                break;
-        }
-    });
+                <label for="length">Length</label>
+                    <input type="number" step="0.01" class="form-control" id="#length required> `);
+  
+    default:
+      return []; 
+  }
+};
+const productType = [
+  {
+    code: "DV",
+    name: "DVD"
+  },
+  {
+    code: "BO",
+    name: "Book"
+  },
+  {
+    code: "FU",
+    name: "Furniture"
+  } 
+];
 
-    $('form').submit(function(e) 
-    {
-        e.preventDefault();
+const createOptionElement = (text, value) => {
+  const element = document.createElement("option");
+  element.textContent = text;
+  element.value = value;
+  return element;
+};
 
-        let inputs = {};
-        $(this).find(':input').each(function() {
-            inputs[$(this).attr("name")] = $(this).val();
-        });
+const createLiElement = text => {
+  const element = document.createElement("li");
+  element.textContent = text;
+  return element;
+};
 
-        $.post('/products/add', inputs, function(data, status) {
-            $('#message').show().removeClass('alert-success alert-danger').addClass(`alert-${data.status}`).html(data.message);
-        });
-    });
+const productTypeElement = document.querySelector("select");
+productTypes.forEach(productType => {
+  productTypeElement.appendChild(createOptionElement(productType.name, productType.code));
+});
+
+productTypeElement.addEventListener("change", e => {
+ 
+  const Type = getType(e.target.value);
+  const TypeElement = document.getElementById("productType");
+  
+  TypeElement.innerHTML = "";
+ 
+  Type.forEach(Type => {
+    TypeElement.appendChild(createLiElement(Type));
+  });
 });
